c274fc3d7831171c57a5aab730b7a0a13ff8806c
Artiom Diomin <kron82@gmail.com>
2008-08-31 14:18:02 +0300

    simplicity template from freecsstemplates.org

--- urls.py
+++ urls.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'base.html'}),
+    
+    (r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
 )
