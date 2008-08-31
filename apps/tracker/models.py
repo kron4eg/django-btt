cfafe2c82883957856380502369df7426a4fe8e8
Artiom Diomin <kron82@gmail.com>
2008-08-31 23:59:50 +0300

    IP for peer

--- apps/tracker/models.py
+++ apps/tracker/models.py
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 class Peer(models.Model):
     torrent = models.ForeignKey('torrent.torrent', verbose_name=_('torrent'))
     peer_id = models.CharField(_('peer ID'), max_length=128, db_index=True)
+    ip = models.IPAddressField(_('IP address'))
     port = models.PositiveIntegerField(_('post'))
     key = models.CharField(_('key'), max_length=255)
     uploaded = models.PositiveIntegerField(_('uploaded'))
