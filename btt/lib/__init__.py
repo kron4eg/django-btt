01ee1960045bc03409533300c230effbf66c9c82
Artiom Diomin <kron82@gmail.com>
2008-11-28 21:26:17 +0200

    cleanup lib

--- btt/lib/__init__.py
+++ btt/lib/__init__.py
@@ -1,6 +1,5 @@
 from bt import bencoding
-from db import load_content_objects, load_generic_related, load_related
 from decorators import render_to
 from slug import slugit
 
-__all__ = ('bencoding', 'slugit', 'render_to', 'load_content_objects', 'load_generic_related', 'load_related')
+__all__ = ('bencoding', 'slugit', 'render_to')
