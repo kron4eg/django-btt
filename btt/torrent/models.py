650deb0afaaef86df5468f7fc7f13fe02c568111
Artiom Diomin <kron82@gmail.com>
2008-11-28 22:43:16 +0200

    django-mptt changed to django-treebeard

--- btt/torrent/models.py
+++ btt/torrent/models.py
@@ -5,12 +5,12 @@ from hashlib import sha1
 from django.db import models
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
+from treebeard.mp_tree import MP_Node
 
-import mptt
-from btt.lib.bt.bencoding import encode, decode
+from btt.lib import bencoding
 
 
-class Category(models.Model):
+class Category(MP_Node):
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', verbose_name=_('Parent'))
     title = models.CharField(_('Title'), max_length=80)
     image = models.ImageField(_('Image'), upload_to='img/category', blank=True, null=True)
@@ -19,16 +19,13 @@ class Category(models.Model):
     def __unicode__(self):
         return smart_unicode(self.title)
 
-    def tree_represent(self, space='&nbsp;', begin_from_level=0):
-        return space * 4 * (self.level - begin_from_level) + ' %s' % self.__unicode__()
-    tree_represent.allow_tags = True
+#    def tree_represent(self, space='&nbsp;', begin_from_level=0):
+#        return space * 4 * (self.level - begin_from_level) + ' %s' % self.__unicode__()
+#    tree_represent.allow_tags = True
 
     class Meta:
         verbose_name = _('category')
         verbose_name_plural = _('categories')
-        ordering = ('lft', 'title')
-
-mptt.register(Category,order_insertion_by=['title'])
 
 
 class Torrent(models.Model):
